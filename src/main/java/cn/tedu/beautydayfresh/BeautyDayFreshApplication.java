474be934e37a618f4d8f8f929d142e13fd9990e4
Administrator <1378703114@qq.com>
2022-06-18 11:38:10 +0800

    Initial commit

---
+++ src/main/java/cn/tedu/beautydayfresh/BeautyDayFreshApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.beautydayfresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyDayFreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyDayFreshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tedu/beautydayfresh/BeautyDayFreshApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.beautydayfresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyDayFreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
